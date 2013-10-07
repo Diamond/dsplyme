0d83f18b8c5d55193336f3f07729da3ee206c267
Brandon Richey <brichey@teladoc.com>
2013-10-07 09:46:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Showoff::Application.config.secret_key_base = 'dc370f18da6946d8142f8f512af1f854f2e471f3d0846de65d00b48389687226283816c45f64079ec65a3fd70a2076148c4c8bc4223ba3a7595958cf4f4a7184'

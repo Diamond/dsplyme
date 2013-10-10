f20ba4eb6bf2978cd02655bf42f4f7431668ea7b
Brandon Richey <brichey@teladoc.com>
2013-10-10 12:15:27 -0400

    Authentication added!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates_confirmation_of :password
+  validates :password, presence: true, on: :create
+  validates :email, presence: true, uniqueness: true
 end
